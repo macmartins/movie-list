cb7d38750b3082981831bd5f50df0e74a49a75cf
macmartins <firewolf949@gmail.com>
2023-06-16 21:50:51 +0100

    - Added build to server - Client can run on server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const movies_routes = require("./routes/movies.js");
+const path = require("path");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
@@ -14,5 +15,10 @@ mongoose
   })
   .catch((err) => console.log(err));
 
+app.use(express.static(path.join(__dirname, "build")));
 app.use(express.json());
 app.use("/api/movies", movies_routes);
+
+app.get("*/", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/build/index.html`));
+});
